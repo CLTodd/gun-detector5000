12762f2facfa9c13dd20df5b56571dfd573ed470
Candace Todd <54723302+CLTodd@users.noreply.github.com>
2022-11-14 02:00:20 -0500

    Update JPEGtoNPY.py

--- JPEGtoNPY.py
+++ JPEGtoNPY.py
@@ -16,15 +16,37 @@ repoPath ="C:\\Users\\CLT5441\\source\\repos\\gun-detector5000\\Pistol classific
 
 Class_Name = [c for c in listdir(repoPath)]
     
-for Class in Class_Name:
-    fullPath = repoPath + Class + "\\"
-    pictureFiles = [f for f in listdir(fullPath) if isfile(join(fullPath, f))]
-    pictures = list()    
-    for picFileName in pictureFiles:
-        pictures.append(imageio.imread(fullPath + picFileName))
-    
-    pictures = np.array(pictures, dtype=object)
-    npyFileName = "X:\\445\\Pistol Classification Arrays\\" + Class + ".npy"
-    print(npyFileName)
-    np.save(npyFileName, pictures)
+listOfAllPictures = []
+labelsForAllPictures = []
+labels = { Class_Name[i] : i for i in range(len(Class_Name)) }
 
+for Class in Class_Name:
+    print(Class)
+    print(labels[Class])
+    # Determine if we're in the gun class
+    if (labels[Class]==0):
+        isGun = 1
+    else:
+        isGun = 0
+    # Find full path of picture files
+    fullPath = repoPath + Class + "\\"
+    # List of all picture file names
+    pictureFiles = [f for f in listdir(fullPath) if isfile(join(fullPath, f))]
+    # Convert each picture file into an array   
+    for picFileName in pictureFiles:
+        # Store the array version of the picture
+        listOfAllPictures.append(imageio.imread(fullPath + picFileName))
+        # Store the class of the picture
+        # Storing it as a list of arrays because  I think (?) that's what the training structure was like in the tutorial I saw
+        labelsForAllPictures.append(np.array(isGun))
+    
+# Conver the lists to np arrays
+listOfAllPictures = np.array(listOfAllPictures, dtype=object)
+labelsForAllPictures = np.array(labelsForAllPictures, dtype=object)
+
+# Save the arrays
+npyPicFileName = "X:\\445\\Pistol Classification Arrays\\full_data.npy"
+npyLabelFileName = "X:\\445\\Pistol Classification Arrays\\full_data_labels.npy"
+
+np.save(npyPicFileName, listOfAllPictures)
+np.save(npyLabelFileName, labelsForAllPictures)
