4f2b454fa5d38b8887a5ab046978c59164133829
Candace Todd <54723302+CLTodd@users.noreply.github.com>
2022-11-28 15:37:07 -0500

    Update JPEGtoNPY.py
    added more comments

--- Our Work/JPEGtoNPY.py
+++ Our Work/JPEGtoNPY.py
@@ -14,16 +14,17 @@ from os.path import isfile, join
 
 repoPath ="C:\\Users\\CLT5441\\source\\repos\\gun-detector5000\\Pistol classification\\"
 
-Class_Name = [c for c in listdir(repoPath)]
+Class_Name = [c for c in listdir(repoPath)] # generates a list of all of the class names that we have images for
     
 listOfAllPictures = []
 labelsForAllPictures = []
-labels = { Class_Name[i] : i for i in range(len(Class_Name)) }
+labels = { Class_Name[i] : i for i in range(len(Class_Name)) } # generates a dictionary with class name as key and the index of that class name in the class_name vector as the value
 
 for Class in Class_Name:
-    print(Class)
-    print(labels[Class])
+    print(Class)         # just for debugging
+    print(labels[Class]) # just for debugging
     # Determine if we're in the gun class
+    # For the purposes of training the binary classifier (gun/not gun), convert the class labels to be binary
     if (labels[Class]==0):
         isGun = 1
     else:
@@ -32,13 +33,13 @@ for Class in Class_Name:
     fullPath = repoPath + Class + "\\"
     # List of all picture file names
     pictureFiles = [f for f in listdir(fullPath) if isfile(join(fullPath, f))]
-    # Convert each picture file into an array   
+    # Convert each picture file into an array of numbers    
     for picFileName in pictureFiles:
-        # Store the array version of the picture
-        listOfAllPictures.append(imageio.imread(fullPath + picFileName))
+        # Convert the image into an array
+        arrayVersionOfPicture = imageio.imread(fullPath + picFileName) # Each pixel in the picture gets 3 numbers, one number for each of the RGB values
+        listOfAllPictures.append(arrayVersionOfPicture) # Store the array version of the picture
         # Store the class of the picture
-        # Storing it as a list of arrays because  I think (?) that's what the training structure was like in the tutorial I saw
-        labelsForAllPictures.append(np.array(isGun))
+        labelsForAllPictures.append(np.array(isGun)) # Storing it as a list of arrays because  I think (?) that's what the training structure was like in the tutorial I saw
     
 # Conver the lists to np arrays
 listOfAllPictures = np.array(listOfAllPictures, dtype=object)
